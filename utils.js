db8aa3e4fe5bd351cb97ee03c8243ca89e1e6f10
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-22 18:56:12 -0400

    Add a couple of reusable functions

--- utils.js
+++ utils.js
@@ -1,4 +1,19 @@
 /**
+ * Transform a string into an array of Audio elements to play them
+ * individually
+ * 
+ * @param { string } word word to be spelled
+ * @param { string } path path where audio files are stored
+ * @returns an array of audio elements
+ */
+function getAudioArray (word, path) {
+  const wordSplited = word.toUpperCase().split('')
+  return wordSplited.map((letter) => {
+    return new Audio(`${path}${letter}.mp3`)
+  })
+}
+
+/**
  * Plays each audio element inside of the array
  * one after the other
  * 
@@ -11,4 +26,13 @@ export function spell(audioArray) {
       audioArray[i].play()
     },{once: true})
   }
+}
+
+/**
+ * 
+ * @param { number } maxNumber 
+ * @returns a pseudo-random number between zero and the maxNumber without including it
+ */
+export function getRandomNum(maxNumber) {
+  return Math.floor(Math.random() * maxNumber);
 }
