8f0e9ff6d3bca44678eec2cec9890edfdea7c321
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-22 18:39:00 -0400

    Grab html elements

---
+++ pages/listen-n-type/listen-n-type.js
@@ -0,0 +1,8 @@
+import { COMMON_WORDS } from '../../constants.js';
+
+const formEl = document.querySelector('#form');
+const validationMessage = document.querySelector('#validation-message');
+const userAnswerInput = document.querySelector('#user-answer-input');
+const playButton = document.querySelector('#play-btn');
+const validateBtn = document.querySelector('#validate-btn');
+const nextBtn = document.querySelector('#next-word');
