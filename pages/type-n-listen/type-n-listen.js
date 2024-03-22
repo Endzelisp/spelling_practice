c0f7a8888499950f737cda876586881bf6869df7
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-22 06:18:47 -0400

    Business logic

---
+++ pages/type-n-listen/type-n-listen.js
@@ -0,0 +1,26 @@
+import { spell } from "../../utils.js";
+
+const formEl = document.querySelector('#form-wrapper');
+const userInputEl = document.querySelector('#user-input');
+const wordSpelledDiplay = document.querySelector('#word-spelled');
+const PATH = '../../assets/letters/';
+
+function getAudioArray (word) {
+  const wordSplited = word.toUpperCase().split('')
+  return wordSplited.map((letter) => {
+    return new Audio(`${PATH}${letter}.mp3`)
+  })
+}
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const userInputValue = userInputEl.value;
+  if (userInputValue === '') return
+  wordSpelledDiplay.textContent = userInputValue;
+  const spellingAudioArray = getAudioArray(userInputValue);
+  spell(spellingAudioArray);
+})
+
+formEl.addEventListener('reset', () => {
+  wordSpelledDiplay.textContent = '';
+})
