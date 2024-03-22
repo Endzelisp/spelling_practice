6216bfcb2ff8f5492f7247207613b4513c5941cc
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-22 19:11:46 -0400

    Wire up logic of play button

--- pages/listen-n-type/listen-n-type.js
+++ pages/listen-n-type/listen-n-type.js
@@ -1,4 +1,5 @@
 import { COMMON_WORDS } from '../../constants.js';
+import { getRandomNum, getAudioArray, spell } from '../../utils.js';
 
 const formEl = document.querySelector('#form');
 const validationMessage = document.querySelector('#validation-message');
@@ -6,3 +7,23 @@ const userAnswerInput = document.querySelector('#user-answer-input');
 const playButton = document.querySelector('#play-btn');
 const validateBtn = document.querySelector('#validate-btn');
 const nextBtn = document.querySelector('#next-word');
+
+const PATH = '../../assets/letters/';
+let currentWord = '';
+
+addEventListener('DOMContentLoaded', () => {
+  const randomIndex = getRandomNum(COMMON_WORDS.length);
+  currentWord = COMMON_WORDS[randomIndex];
+
+  playButton.addEventListener('click', () => {
+    const audioArray = getAudioArray(currentWord, PATH);
+    spell(audioArray)
+  })
+})
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (userAnser === '') return
+  const userAnser = userAnswerInput.value;
+
+})
