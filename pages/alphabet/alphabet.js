7eb058eb46eddd90e2cf046633a8d0784f238d6e
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-22 21:22:28 -0400

    Avoid showing the underscore that represent the silence

--- pages/alphabet/alphabet.js
+++ pages/alphabet/alphabet.js
@@ -4,17 +4,21 @@ const alphabetWrapper = document.querySelector('.alphabet-wrapper');
 
 const PATH = '../../assets/letters/';
 
+const alphabetWithoutSilence = ALPHABET.filter((letter) => letter !== '_')
+
 function getAlphabetAudioArray(alphabet, path) {
   return alphabet.map((letter) =>  new Audio(`${path}${letter}.mp3`))
 }
 
-const alphabetAudioArray = getAlphabetAudioArray(ALPHABET, PATH);
+const alphabetAudioArray = getAlphabetAudioArray(alphabetWithoutSilence, PATH);
 
 const lettersButtonsArray = alphabetAudioArray.map((audioLetter, index) => {
   const btn = document.createElement('button');
   btn.type = 'button';
   btn.classList.add('letter');
-  btn.textContent = ALPHABET[index];
+  // Added one to the index to avoid including the underscore
+  // symbol that represent the silence
+  btn.textContent = ALPHABET[index + 1];
 
   btn.append(audioLetter);
   btn.addEventListener('click', () => {
